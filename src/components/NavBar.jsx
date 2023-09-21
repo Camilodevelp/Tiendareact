6c3937e60e9a0e54eb861631cc6ac642d632f879
camilo <kmiloeduardo@gmail.com>
2023-09-21 11:17:44 -0500

    implementacion navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,49 +1,24 @@
 import Container from "react-bootstrap/Container";
-import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
 
-import { CartWidget } from "./CartWidget";
-import data from "../data/productos.json";
 
-export const NavBar = (props) => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const db = getFirestore();
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link as = {Link} to = "{/categoryID/Camisas} " >Camisas</Nav.Link>
+            <Nav.Link href="#pricing">Pantalones</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
 
-    const refCollection = collection(db, "Items");
 
-    getDocs(refCollection).then((snapshot) => {
-      if (snapshot.size === 0) console.log("no results");
-      else
-        setProducts(
-          snapshot.docs.map((doc) => {
-            return { id: doc.id, ...doc.data() };
-          })
-        );
-    });
-  }, []);
-  const categories = data.map((product) => product.categoryID);
-  const uniqueCategories = new Set(categories);
-
-  <Navbar bg="dark" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand href="#home">Mi Tienda</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        {[...uniqueCategories].map((categoryID) => (
-          <Nav.Link
-            as={NavLink}
-            key={categoryID}
-            to={`/category/${categoryID}`}
-          >
-            {categoryID}
-          </Nav.Link>
-        ))}
-      </Nav>
-    </Container>
-    <CartWidget />
-  </Navbar>;
-};
